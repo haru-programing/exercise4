e3147f7450546ad3ed9fe62eab50ee5d4530ab92
永嶺　明 <haru@eireiakiranoMacBook-Air.local>
2020-08-05 09:13:13 +0900

    HoT:Initial revision

---
+++ Head.tails.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+  int h=0,t=0;
+  int i,r;
+
+  printf("Tossing a coin...\n");
+
+  for(i=0;i<3;i++) {
+    printf("Round%d: ",i+1);
+    r = rand()%2;
+    if(r==0) {
+      printf("Tails\n");
+      t++;
+    } else {
+      printf("Heads\n");
+      h++;
+    }
+  }
+  printf("Heads:%d,Tails:%d\n",h,t);
+
+  return 0;
+
+}
